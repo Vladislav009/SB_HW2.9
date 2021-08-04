93726d386f3c2708d384a349abd8f838377d3e64
Vladislav <developer@arthurandco.org>
2021-08-04 17:28:19 +0500

    finish animation

---
+++ SB_HW2.9/Models/Animation.swift
@@ -0,0 +1,17 @@
+//
+//  Animation.swift
+//  SB_HW2.9
+//
+//  Created by Vladislav Kulak on 04.08.2021.
+//
+
+import Spring
+
+struct Animation {
+    let animationType  = Spring.AnimationPreset.allCases
+    var currentRandom = ""
+    var nextRandom = ""
+    
+}
+
+
--- SB_HW2.9/ViewController.swift
+++ SB_HW2.9/ViewController.swift
@@ -5,15 +5,46 @@
 //  Created by Vladislav Kulak on 04.08.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    private var animation = Animation()
+    
+    @IBOutlet weak var mainView: SpringView!
+    @IBOutlet weak var runButton: SpringButton!
+    @IBOutlet weak var currentAnimationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainView.layer.cornerRadius = 10
+        runButton.layer.cornerRadius = 10
+        
     }
-
-
+    
+    @IBAction func runSpringButton(_ sender: SpringButton) {
+        currentAnimationLabel.text = animation.currentRandom
+        mainView.animation = animation.currentRandom
+        runButton.setTitle(getNextRandom(), for: .normal)
+        mainView.force = 2
+        mainView.duration = 1
+        mainView.delay = 0.3
+        mainView.animate()
+    }
 }
+
+extension ViewController {
+    func getRandomType() -> String {
+        return animation.animationType.randomElement()?.rawValue ?? "Pop"
+    }
+    
+    func getNextRandom() -> String {
+        animation.nextRandom = getRandomType()
+        animation.currentRandom = animation.nextRandom
+        return animation.nextRandom
+    }
+}
+
+
 
